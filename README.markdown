09d0d1ec4461440ebcaf62335022f744db31cb1d
Michael Stewart <michael@stewart.io>
2013-12-08 06:21:04 +0000

    initial commit

---
+++ README.markdown
@@ -0,0 +1,14 @@
+## Readme
+
+### Project Details
+
+#### General Idea:
+I'm working on app that will allow users to purchase a physical 'subscription' to their Instagram feeds for their grandparents. Although my parents and grandparents do use the internet regularly, they don't follow me on social networks so in order for me to share with them, I have to specifically go about emailing them a photo. I find that I upload photos to Instagram every few weeks. However, I only email my grandparents once or twice a year.
+ 
+#### Use case:
+I'd like to use the app to automatically keep my grandparents in the loop. I know that it will make my grandparent's day every time they receive a photo in the mail and after a few months they'll have quite a collection. This will all only cost about $1 each photo. I was actually going to setup something similar just for myself using the Instagram and lob.com APIs without a front end in order to achieve this.
+
+### Please see:
+
+- /applications/init/  - Main site folder
+- /gluon/contrib/login_methods/instagram_account.py - Code for connecting to instagram for OAuth
---
+++ gluon/contrib/login_methods/instagram_account.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of web2py Web Framework (Copyrighted, 2007-2009).
+Developed by Massimo Di Pierro <mdipierro@cs.depaul.edu>.
+License: GPL v2
+
+Thanks to Hans Donner <hans.donner@pobox.com> for GaeGoogleAccount.
+"""
+
+from gluon.http import HTTP
+try:
+    from instagram import client
+except ImportError:
+    raise HTTP(400, "instagram module not found")
+
+
+class InstagramAccount(object):
+    """
+    Login will be done via Google's Appengine login object, instead of web2py's
+    login form.
+
+    Include in your model (eg db.py)::
+
+        from gluon.contrib.login_methods.linkedin_account import LinkedInAccount
+        auth.settings.login_form=LinkedInAccount(request,KEY,SECRET,RETURN_URL)
+
+    """
+
+    def __init__(self, request, client_id=None, client_secret=None, access_token=None, redirect_uri=None):
+        self.request = request
+        self.api = client.InstagramAPI(client_id, client_secret, access_token, redirect_uri)
+
+    def login_url(self, next="/"):        
+        return self.api.get_authorize_url(scope=["likes","comments"])
+
+    def logout_url(self, next="/"):
+        return ''
+
+    def get_user(self):
+        result = self.request.vars.code and self.api.exchange_code_for_access_token(self.request.vars.code)
+        print result
+        if result:
+            name = result[1]['full_name']
+            rdict = dict(first_name=name,
+                username=result[1]['username'])
+            print rdict
+            return rdict
+
